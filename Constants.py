df841e481761a2a1b60edb784c7c2aea54becb2c
Tuna <tunacici7@gmail.com>
2022-04-07 22:37:36 +0300

    Added new control mechanisms.

---
+++ Constants.py
@@ -0,0 +1,47 @@
+import enum
+import numpy
+
+class Mode(enum.Enum):
+    PLOTTING = 0
+    SIMULATION = 1
+    INTEGRATION = 2
+
+class State(enum.Enum):
+    STATIONARY = 0
+    IDLING = 1
+    TAKING_OFF = 2
+    HOVERING = 3
+    LANDING = 4
+    ON_MISSION = 5
+
+class Mission(enum.Enum):
+    NONE = 0
+    FORMING = 1
+    MOVING = 2
+    ROTATING = 3
+
+class FormationShape(enum.Enum):
+    POINT = 0
+    LINE = 1
+    TRIANGLE = 2
+    SQUARE = 3
+
+class MissionInfo:
+    otherAgents: list
+    formationShape: FormationShape
+    minimumSafeDistance: float 
+    targetPoint: numpy.ndarray
+    rotateAngle: float
+    angularVelocity: float
+    maxVelocity: float
+
+    def __init__(self,
+        otherAgents: list,
+        formationShape: FormationShape,
+        minimumSafeDistance: float,
+        targetPoint: numpy.ndarray,
+        rotateAngle: float,
+        angularVelocity: float,
+        maxVelocity: float) -> None:
+
+MODE = Mode.PLOTTING
---
+++ Main.py
@@ -0,0 +1,30 @@
+import time
+
+from Agent import Agent
+
+AGENT_COUNT = 3
+RUNNING = True
+TICK_RATE = 60 # Hz
+
+if __name__ == "__main__":
+    agents = []
+    currTime = time.perf_counter()
+    lastTime = time.perf_counter()
+    deltaTime = 0.0
+
+    # Create new agents
+    for i in range(AGENT_COUNT):
+        agents.append(Agent(i))
+
+    while RUNNING:
+        currTime = time.perf_counter()
+        deltaTime = currTime - lastTime
+        if deltaTime < 1.0 / TICK_RATE:
+            continue
+
+        for agent in agents:
+            agent.tick(deltaTime)
+
+        print(agents[i])
+
+        lastTime = time.perf_counter()
