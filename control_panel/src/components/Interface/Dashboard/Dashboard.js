0883dd41c418e53ffcd109043a6d3d2746a8ca3d
Tuna <tunacici7@gmail.com>
2022-04-27 16:30:18 +0300

    Basic layout.

---
+++ control_panel/src/components/Interface/Dashboard/Dashboard.js
@@ -0,0 +1,24 @@
+import React  from 'react';
+import Plot from 'react-plotly.js'
+
+import "./Dashboard.css"
+
+function Dashboard() {
+    return (
+        <div className='Dashboard'>
+            <h2 className='Dashboard-Title'>
+                Dashboard
+            </h2>
+            <div className='Dashboard-Container'>
+                <div className='Dashboard-Map'>
+
+                </div>
+                <div className='Dashboard-'>
+
+                </dv>
+            </div>
+        </div>
+    );
+}
+
+export default Dashboard;
