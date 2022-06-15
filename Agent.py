179f8ea7527dd15a586ea38c1cc5097d15aa7b1f
tunacici <tunacici7@gmail.com>
2022-06-15 23:03:05 +0300

    House keeping TM.

--- Agent.py
+++ Agent.py
@@ -14,25 +14,18 @@ from crazyflie_driver.msg import FullState
 from std_msgs.msg import Empty
 from geometry_msgs.msg import Twist
 
-# TODO: Fix the private values "_" to "__"
-
 class Agent:
     """This class represents the real-world agent.
     It does swarming and other operations to control the agent.
     """
+    __crazyflie: Crazyflie
     __name: str
-    __address: str
-    _status:  bool
-    _state: str
+    __state: str
     
     __initialPos: np.ndarray
     __pos: np.ndarray
     __vel: np.ndarray
     __speed: float
-    
-    __pitch: float
-    __yaw: float
-    __roll: float
 
     # Swarm related
     __isFormationActive: bool
@@ -41,35 +34,27 @@ class Agent:
     __isSwarming: bool
     __formationMatrix: np.ndarray
 
-    _targetPoint: np.ndarray
-
-    __crazyflie: Crazyflie
+    __targetPoint: np.ndarray
     
     # time points
     __t1: float
     __t2: float
-    __maxVel: float 
     
     # position points
     __x1: np.ndarray
     __x2: np.ndarray
-    
-    # validity
-    __validtyCount: int
 
     # The publisher to be able to control in position
-    __pubSetpointPos: rospy.Publisher
     __pubHover: rospy.Publisher
     __pubStop: rospy.Publisher
     __pubFullState: rospy.Publisher
-    __pubStop: rospy.Publisher
     __pubTwist: rospy.Publisher
 
     # Message structures
     __hoverMsg: Hover
     __fullStateMsg: FullState
     
-    def __init__(self, cf: Crazyflie, initialPos: np.ndarray, name: str, address: str) -> None:
+    def __init__(self, cf: Crazyflie, initialPos: np.ndarray, name: str) -> None:
         """Initializes the agent class.
 
         Args:
@@ -79,9 +64,6 @@ class Agent:
         """
         self.__crazyflie = cf
         self.__name = name
-        self.__address = address
-
-        self.__maxVel = 0.3
         
         self.__isFormationActive = False
         self.__isAvoidanceActive = False
@@ -89,14 +71,13 @@ class Agent:
         self.__isSwarming = False
         self.__formationMatrix = np.array([0.0])
 
-        self._targetPoint = np.array([0.0, 0.0, 0.0])
+        self.__targetPoint = np.array([0.0, 0.0, 0.0])
         
-        self._status = False
-        self._state = "STATIONARY"
+        self.__state = "STATIONARY"
         
         self.__initialPos = np.array(initialPos)
         self.__pos = np.array(initialPos)
-        self.__vel= np.array([0.0, 0.0, 0.0])
+        self.__vel = np.array([0.0, 0.0, 0.0])
         self.__speed = 0.0
         
         self.__pitch= 0.0
@@ -116,24 +97,47 @@ class Agent:
         rospy.Subscriber(self.__name + "/external_position" , GenericLogData , self.__externalPositionCallback)
 
         # The publisher to be able to control in position
-        self.__pubSetPointPos = rospy.Publisher(self.__name + "/cmd_position", Position , queue_size=10)
         self.__pubHover = rospy.Publisher(self.__name + "/cmd_hover", Hover , queue_size=10)
         self.__pubStop = rospy.Publisher(self.__name + "/cmd_stop", Empty , queue_size=10)
         self.__pubFullState = rospy.Publisher(self.__name + "/cmd_full_state", FullState , queue_size=10)
-        self.__pubStop = rospy.Publisher(self.__name + "/cmd_stop", Empty , queue_size=10)
         self.__pubTwist= rospy.Publisher(self.__name + "/cmd_vel", Twist , queue_size=10)
-        rospy.sleep(1 / 10)
+        rospy.sleep(0.1)
 
         # Message structures
         self.__hoverMsg = Hover()
         self.__fullStateMsg = FullState()
-        self.__PointMsg = Position()
 
-        # Activate service controls
-        self.__crazyflie.setParam("commander/enHighLevel", 1)
+    def __localPositionCallback(self, msg):
+        self.__pos[0] = msg.values[0]
+        self.__pos[1] = msg.values[1]
+        self.__pos[2] = msg.values[2]
+
+        # Update agent info
+        self.__x1 = np.array(self.__x2)
+        self.__x2 = np.array(self.__pos)
+
+        self.__t2 = time.perf_counter()
+        # print(round(self.__t2, 6))
+        self.__vel = (self.__x2 - self.__x1) / (self.__t2 - self.__t1)
+        self.__t1 = time.perf_counter()
         
-    def __str__(self):
-        return f"{self.__name}, {self.__address}, {self.__pos}"
+        self.__speed = Settings.getMagnitude(self.__vel)
+
+    def __externalPositionCallback(self, msg):
+        return
+        self.__pos[0] = msg.values[0]
+        self.__pos[1] = msg.values[1]
+        self.__pos[2] = msg.values[2]
+
+        # Update agent info
+        self.__x1 = self.__x2
+        self.__x2 = self.__pos
+        
+        self.__t2 = time.perf_counter()
+        self.__vel = (self.__x2 - self.__x1) / (self.__t2 - self.__t1)
+        self.__t1 = time.perf_counter()
+        
+        self.__speed = Settings.getMagnitude(self.__vel)  
 
     def __formationControl(self, agents: list) -> np.ndarray:
         """Calculates the formation 'force' to be applied to the agent.
@@ -200,47 +204,13 @@ class Agent:
         else:
             swarmCenter = self.getPos()
 
-        retValue = self._targetPoint - swarmCenter
+        retValue = self.__targetPoint - swarmCenter
         
-        # max velocity 1.0
-        if self.__maxVel < Settings.getMagnitude(retValue):
-            retValue = Settings.setMagnitude(retValue, self.__maxVel)
+        # max velocity 0.5
+        if 0.5 < Settings.getMagnitude(retValue):
+            retValue = Settings.setMagnitude(retValue, 0.5)
         
         return retValue
-
-    def __localPositionCallback(self, msg):
-        self.__pos[0] = msg.values[0]
-        self.__pos[1] = msg.values[1]
-        self.__pos[2] = msg.values[2]
-
-        # Update agent info
-        self.__x1 = np.array(self.__x2)
-        self.__x2 = np.array(self.__pos)
-
-        self.__t2 = time.perf_counter()
-        # print(round(self.__t2, 6))
-        self.__vel = (self.__x2 - self.__x1) / (self.__t2 - self.__t1)
-        self.__t1 = time.perf_counter()
-        
-        self.__speed = Settings.getMagnitude(self.__vel)
-
-    def __externalPositionCallback(self, msg):
-        return
-        self.__pos[0] = msg.values[0]
-        self.__pos[1] = msg.values[1]
-        self.__pos[2] = msg.values[2]
-
-        print("external")
-
-        # Update agent info
-        self.__x1 = self.__x2
-        self.__x2 = self.__pos
-        
-        self.__t2 = time.perf_counter()
-        self.__vel = (self.__x2 - self.__x1) / (self.__t2 - self.__t1)
-        self.__t1 = time.perf_counter()
-        
-        self.__speed = Settings.getMagnitude(self.__vel)  
 
     def getName(self) -> str:
         """Returns the name of the agent.
@@ -250,23 +220,6 @@ class Agent:
         """
         return self.__name
     
-    def getAddress(self) -> str:
-        """Returns the address of the agent.
-
-        Returns:
-            str: Address of the agent.
-        """
-        return self.__address
-    
-    def getStatus(self) -> bool:
-        """Returns status name of the agent.
-
-        Returns:
-            bool: Status of the agent.
-        """
-
-        return "online"
-    
     def getState(self) -> str:
         """_summary_
 
@@ -274,7 +227,7 @@ class Agent:
             str: State of the agent
         """
         
-        return self._state
+        return self.__state
     
     def getPos(self) -> np.ndarray:
         """Returns the position of the agent. (Vector3)
@@ -302,32 +255,6 @@ class Agent:
         
         return Settings.getMagnitude(self.__vel)
     
-    def getPitch(self) -> float:
-        """Returns the pitch of the agent.
-
-        Returns:
-            float: Pitch of the agent.
-        """
-        return self.__pitch
-    
-    def getYaw(self) -> float:
-        """Returns the yaw of the agent.
-
-        Returns:
-            float: Yaw of the agent.
-        """
-        (self.__roll, self.__pitch, self.__yaw) = self.__crazyflie.rpy()
-        return self.__yaw
-    
-    def getRoll(self) -> float:
-        """Returns the row of the agent.
-
-        Returns:
-            float: Row of the agent.
-        """
-        (self.__roll, self.__pitch, self.__yaw) = self.__crazyflie.rpy()
-        return self.__roll
-    
     def getInitialPos(self) -> np.ndarray:
         """Returns the initial position of the agent.
 
@@ -337,14 +264,9 @@ class Agent:
         return self.__initialPos
     
     def setTargetPoint(self, target) -> bool:
-        self._targetPoint = target
-        self._state = "MOVING"
+        self.__targetPoint = target
+        self.__state = "MOVING"
         
-        return True
-    
-    def setMaxVel(self, vel) -> bool:
-        self.__maxVel = vel
-
         return True
 
     def setFormationActive(self, status: bool) -> bool:
@@ -387,7 +309,6 @@ class Agent:
 
         if self.__isFormationActive:
             formationVel = self.__formationControl(agents)
-            formationVel = Settings.setMagnitude(formationVel, 0.5)
 
         if self.__isAvoidanceActive:
             avoidanceVel = self.__avoidanceControl()
@@ -396,74 +317,31 @@ class Agent:
             trajectoryVel = self.__trajectoryControl(agents)
 
         controlVel = formationVel + avoidanceVel + trajectoryVel
-        if 0.06 <= Settings.getMagnitude(controlVel):
-            controlVel = Settings.setMagnitude(controlVel, 0.06)
-
-        isHighSpeed = False if (self.__speed <= 0.05) else True
-        isHighControl = False if (Settings.getMagnitude(controlVel) <= 0.05) else True
-
-        # Calculate is states
-        if self._state == "TAKING_OFF" and 0.49 <= self.__pos[2]:
-            self._state = "HOVERING"
-            if self.__validtyCount < 0:
-                self.__validtyCount -= 1
-        elif self._state == "LANDING" and self.__pos[2] <= 0.15 and self._targetPoint[2] <= 0.10:
-            self._state = "STATIONARY"
-            if self.__validtyCount < 0:
-                self.__validtyCount -= 1
-        elif self._state == "STATIONARY":
-            pass
-        elif self._state != "TAKING_OFF" and not isHighSpeed and not isHighControl:
-            self.__validtyCount += 1
-            if 180 <= self.__validtyCount:
-                self._state = "HOVERING"
-                self.__validtyCount = 0
-        elif isHighSpeed and isHighControl:
-            self._state = "MOVING"
-            if self.__validtyCount < 0:
-                self.__validtyCount -= 1
-        
-        print(self._state)
-        # print(round(self.__speed, 4))
 
         # Send the message
         if isActive:
-            # Hover Messages
+            # Hover
             self.__hoverMsg.header.frame_id = 'world'
             self.__hoverMsg.header.seq += 1
             self.__hoverMsg.header.stamp = rospy.Time.now()
-            self.__hoverMsg.vx = 0.0
-            self.__hoverMsg.vy = 0.0
+            self.__hoverMsg.vx = controlVel[0]
+            self.__hoverMsg.vy = controlVel[1]
             self.__hoverMsg.yawrate = 0.0
-            self.__hoverMsg.zDistance = self._targetPoint[2]
+            self.__hoverMsg.zDistance = 0.5
 
-            # Full State Messages
+            # Full State
             self.__fullStateMsg.header.frame_id = 'world'
             self.__fullStateMsg.header.seq += 1
             self.__fullStateMsg.header.stamp = rospy.Time.now()
             self.__fullStateMsg.pose.position.x = self.__pos[0] + controlVel[0]
             self.__fullStateMsg.pose.position.y = self.__pos[1] + controlVel[1]
-            self.__fullStateMsg.pose.position.z = self._targetPoint[2]
+            self.__fullStateMsg.pose.position.z = 0.5
 
-            # Services
-            if self._state == "MOVING":
-                goal =  np.array(
-                    [
-                        round(self.__pos[0] + controlVel[0], 2),
-                        round(self.__pos[1] + controlVel[1], 2),
-                        round(self.__pos[2] + controlVel[2], 2)
-                    ]   
-                )
-                # logging.info(f"{self.__name} goal: {goal}")
-
+            # Publish
+            if self.__state == "MOVING":
                 self.__pubFullState.publish(self.__fullStateMsg)
-            elif self._state == "HOVERING":
+            elif self.__state == "HOVERING":
                 self.__pubHover.publish(self.__hoverMsg)
-
-            # self.__pubSetPointPos.publish(self.__PointMsg)
-
-            # Log info
-            # logging.info(f"{self.__name} pos: {self.__pos} target: {self._targetPoint} controlVel: {controlVel}")
 
         return retValue
 
@@ -476,14 +354,9 @@ class Agent:
         Returns:
             bool: Whether the operation was succesfull or not.
         """
-        retValue = False
+        retValue = True
         
-        try:
-            self.__crazyflie.takeoff(targetHeight=height, duration=2.0)
-            self._state = "TAKING_OFF"
-            retValue = True
-        except Exception as e:
-            print(e.with_traceback())
+        self.__crazyflie.takeoff(targetHeight=height, duration=2.0)
     
         return retValue
     
@@ -493,13 +366,10 @@ class Agent:
         Returns:
             bool: Whether the operation was succesfull or not.
         """
-        retValue = False
-        try:
-            self.__crazyflie.land(0.0, duration=3.0)
-            self._state = "LANDING"
-            retValue = True
-        except Exception as e:
-            print(e.with_traceback())
+        retValue = True
+
+        self.__crazyflie.land(0.0, duration=2.0)
+
         return retValue
     
     def goToAsync(self, x: float, y: float, z: float) -> bool:
@@ -513,32 +383,17 @@ class Agent:
         Returns:
             bool: Whether the operation was succesfull or not.
         """
-        retValue = False
+        retValue = True
 
         self.__crazyflie.goTo(
                 goal=[x, y, z],
                 yaw=0.0,
-                duration=4.0,
+                duration=3.0,
                 relative=False
             )
-        self._state = "MOVING"
-        
+
         return retValue
     
-    def goToSync(self, x: float, y: float, z: float) -> bool:
-        """Moves the agent to the desired point. Blocks the flow of the program.
-
-        Args:
-            x (float): X value of the point.
-            y (float): Y value of the point
-            z (float): Z value of the point.
-
-        Returns:
-            bool: Whether the operation was succesfull or not.
-        """
-        retValue = False
-
-        return retValue
 
     def kill(self) -> bool:
         """Stops all the agen motors.
@@ -546,9 +401,9 @@ class Agent:
         Returns:
             bool: Whether the operation was succesfull or not.
         """
-        retValue = False
+        retValue = True
 
-        for i in range(5):
+        for i in range(10):
             self.__pubStop.publish(Empty())
 
         return retValue
--- Main.py
+++ Main.py
@@ -58,12 +58,7 @@ def main() -> None:
     """
     # Initialization
     i = 0
-    agentCount = 6
-    cfIds = []
     agents = []
-    names = []
-    addresses = []
-    statuses = []
 
     # Fix the issue where rospy disables the logging
     os.environ['ROS_PYTHON_LOG_CONFIG_FILE'] = "`rospack find rosgraph`/conf/python_logging.yaml"
@@ -80,28 +75,16 @@ def main() -> None:
     agents.append(Agent(
                 cf=crazyflie.Crazyflie(f"cf{1}", f"/cf{1}"),
                 initialPos= np.array([0.0, -1.0, 0.0]),
-                name=f"cf{1}",
-                address=f"radio:/{1}//80/2M - Unknown"
+                name=f"cf{1}"
             ))
-    names.append("cf1")
 
-
-    logging.info(f"Created agents '{names}'.")
-
-    # Initializing redis
-    logging.info("Initializing Redis.")
-    redisClient = redis.Redis()
-    redisClient.set("agents", json.dumps({"names": names, "addresses": addresses}))
-    redisSub = redisClient.pubsub()
-    redisSub.subscribe(["requests"])
-    
+    logging.info(f"Created agents.")
     logging.info("Creating an instance of 'MissionControl'.")
 
     # Creating mission control    
     missionControl = MissionControl(
         rospyRate=rospyRate,
-        agents=agents,
-        redisClient=redisClient
+        agents=agents
     )
     
     # Start the watchdog
@@ -111,135 +94,41 @@ def main() -> None:
     i = 0
 
     logging.info("All ready! Listening... Press 'q' to exit.")
-    while True:
-        # Read incoming messages from redis
-        try:
-            msg = redisSub.get_message()
-        except KeyboardInterrupt as e:
-            logging.info("Keyboard interrupt detected.")
-            emergencyExit()
 
-        mission = ""
-        target = ""
-        
-        # Parse the message
-        if msg is not None and msg["type"] == "message":
-            parsed = json.loads(msg['data'])
-
-            mission = parsed.get("mission", None)
-            if mission:
-                logging.info(f"Incoming misson request: {mission}")
-
-            target = parsed.get("target", None)
-            if target:
-                logging.info(f"Incoming target: {target}")
-        else:
-            if "trajectory_test" in sys.argv:
-                mission = "mission_trajectory_test"
-                sys.argv.remove("trajectory_test")
-            elif "takeoff_land_test" in sys.argv:
-                mission = "mission_takeoff_land_test"
-                sys.argv.remove("takeoff_land_test")
-            elif "takeoff_test" in sys.argv:
-                mission = "mission_takeoff_test"
-                sys.argv.remove("takeoff_test")
-            elif "takeoff_all_test" in sys.argv:
-                mission = "mission_takeoff_all_test"
-                sys.argv.remove("takeoff_all_test")
-            elif "land_test" in sys.argv:
-                mission = "mission_land_test"
-                sys.argv.remove("land_test")
-            elif "land_all_test" in sys.argv:
-                mission = "mission_land_all_test"
-                sys.argv.remove("land_all_test")
-            elif "formation_test" in sys.argv:
-                mission = "mission_formation_test"
-                sys.argv.remove("formation_test")
-            elif "position_test" in sys.argv:
-                mission = "mission_position_test"
-                sys.argv.remove("position_test")
-            elif "local_rotation_test" in sys.argv:
-                mission = "mission_local_rotation_test"
-                sys.argv.remove("local_rotation_test")
-
-        # Launch a mission if a message exists
-        if mission == "mission_takeoff_all":
-            missionControl.takeOffAll()
-
-        elif mission == "mission_land_all":
-            missionControl.landAll()
-
-        elif mission == "mission_takeoff" and target:
-            missionControl.takeOffAgent(target=target)
-
-        elif mission == "mission_land" and target:
-            missionControl.landAgent(target=target)
-
-        elif mission == "mission_one":
-            missionControl.missionOne()
-        
-        elif mission == "mission_trajectory_test":
-            # TODO: To it in a loop
-            missionControl.takeOffAgent(target=names[0])
-            missionControl.goToAgent(
-                target="cf1",
-                points=[
-                    Point(0.5, 0.0, 0.5, False),
-                    Point(0.0, 0.0, 0.5, False),
-                    Point(-0.5, 0.0, 0.5, False),
-                    Point(0.0, 0.0, 0.5, False),
-                    Point(0.0, 0.5, 0.5, False),
-                    Point(0.0, 0.0, 0.5, False),
-                    Point(0.0, -0.5, 0.5, False),
-                    Point(0.0, 0.0, 0.5, False),
-                    Point(0.0, 0.0, 1.0, False),
-                    Point(0.0, 0.0, 0.5, False),
-                ],
-                maxVel=0.5
-            )
-            missionControl.landAgent(target=names[0])
-        
-        elif mission == "mission_takeoff_land_test":
-            i = 0
-            while i < 3:
-                i += 1
-                missionControl.takeOffAgent(names[0])
-                rospy.sleep(5)
-                missionControl.landAgent(names[0])
-                rospy.sleep(5)
-
-        elif mission == "mission_takeoff_test":
-            # TODO: To it in a loop
-            missionControl.takeOffAgent(names[0])
-
-        elif mission == "mission_takeoff_all_test":
-            # TODO: To it in a loop
-            missionControl.takeOffAll()
-        
-        elif mission == "mission_land_test":
-            # TODO: To it in a loop
-            missionControl.landAgent(names[0])
-
-        elif mission == "mission_land_all_test":
-            # TODO: To it in a loop
-            missionControl.landAll()
-        elif mission == "mission_formation_test":
-            for i, agent in enumerate(agents):
-                logging.info(f"Index: {i}, agent: {agent.getName()}")
-                
-            missionControl.takeOffAll()
-            time.sleep(5.0)
-            missionControl.testFormation()
-            rospy.sleep(5)
-            missionControl.landAll()
-        
-        elif mission == "mission_position_test":
-            missionControl.testPosition()
-
-        elif mission == "mission_local_rotation_test":
-            # TODO: To it in a loop
-            missionControl.takeOffAgent(names[0])
-        
+    if "trajectory_test" in sys.argv:
+        missionControl.takeOffAgent(target=agent[0])
+        missionControl.goToAgent(
+            target="cf1",
+            points=[
+                Point(0.5, 0.0, 0.5, False),
+                Point(0.0, 0.0, 0.5, False),
+                Point(-0.5, 0.0, 0.5, False),
+                Point(0.0, 0.0, 0.5, False),
+                Point(0.0, 0.5, 0.5, False),
+                Point(0.0, 0.0, 0.5, False),
+                Point(0.0, -0.5, 0.5, False),
+                Point(0.0, 0.0, 0.5, False),
+                Point(0.0, 0.0, 1.0, False),
+                Point(0.0, 0.0, 0.5, False),
+            ],
+            maxVel=0.5
+        )
+        missionControl.landAgent(target=agent[0])
+    elif "takeoff_land_test" in sys.argv:
+        missionControl.takeOffAll()
+        rospy.sleep(5)
+        missionControl.landAll(agent[0])
+        rospy.sleep(5)
+    elif "formation_test" in sys.argv:
+        for i, agent in enumerate(agents):
+            logging.info(f"Index: {i}, agent: {agent.getName()}")
+            
+        missionControl.takeOffAll()
+        rospy.sleep(5)
+        missionControl.testFormation()
+        rospy.sleep(5)
+        missionControl.landAll()
+            
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
     main()
