b0b633857d98303e4f6532f727cc013e74b49bad
yasin-herken <yasin.herken123@gmail.com>
2022-06-27 20:37:10 +0300

    Crazyswarm without formation sycn

--- Deprecated/PyCrazySwarm/Main.py
+++ Deprecated/PyCrazySwarm/Main.py
@@ -53,9 +53,7 @@ def main() -> None:
     i = 0
     cfIds = []
     agents = []
-    names = []
-    addresses = []
-    statuses = []
+    names= []
     configFile = "./crazyflies.yaml"
 
     logging.info(f"Reading config file '{configFile}'.")
@@ -75,21 +73,13 @@ def main() -> None:
             Agent(
                 cf=agent,
                 name=f"Agent {cfIds[i]}",
-                address=f"radio:/{cfIds[i]}/125/2M"
+                address=f"radio:/{cfIds[i]}/80/2M"
             )
         )
         names.append(f"Agent {cfIds[i]}")
-        addresses.append(f"radio:/{cfIds[i]}/125/2M")
         i+=1
 
     logging.info(f"Created agents '{names}'.")
-
-    # Initializing redis
-    logging.info("Initializing Redis.")
-    redisClient = redis.Redis()
-    redisClient.set("agents", json.dumps({"names": names, "addresses": addresses}))
-    redisSub = redisClient.pubsub()
-    redisSub.subscribe(["requests"])
     
     logging.info("Creating an instance of 'MissionControl'.")
 
@@ -97,7 +87,6 @@ def main() -> None:
     missionControl = MissionControl(
         crazySwarm=crazySwarm,
         agents=agents,
-        redisClient=redisClient
     )
     
     # Start the watchdog
@@ -109,48 +98,31 @@ def main() -> None:
     logging.info("All ready! Listening... Press 'q' to exit.")
     while True:
         # Read incoming messages from redis
-        try:
-            msg = redisSub.get_message()
-        except KeyboardInterrupt as e:
-            logging.info("Keyboard interrupt detected.")
-            emergencyExit(crazySwarm)
 
         mission = ""
         target = ""
         
-        # Parse the message
-        if msg is not None and msg["type"] == "message":
-            parsed = json.loads(msg['data'])
-
-            mission = parsed.get("mission", None)
-            if mission:
-                logging.info(f"Incoming misson request: {mission}")
-
-            target = parsed.get("target", None)
-            if target:
-                logging.info(f"Incoming target: {target}")
-        else:
-            if "trajectory_test" in sys.argv:
-                mission = "mission_trajectory_test"
-                sys.argv.remove("trajectory_test")
-            elif "takeoff_land_test" in sys.argv:
-                mission = "mission_takeoff_land_test"
-                sys.argv.remove("takeoff_land_test")
-            elif "takeoff_test" in sys.argv:
-                mission = "mission_takeoff_test"
-                sys.argv.remove("takeoff_test")
-            elif "takeoff_all_test" in sys.argv:
-                mission = "mission_takeoff_all_test"
-                sys.argv.remove("takeoff_all_test")
-            elif "land_test" in sys.argv:
-                mission = "mission_land_test"
-                sys.argv.remove("land_test")
-            elif "land_all_test" in sys.argv:
-                mission = "mission_land_all_test"
-                sys.argv.remove("land_all_test")
-            elif "formation_test" in sys.argv:
-                mission = "mission_formation_test"
-                sys.argv.remove("formation_test")
+        if "trajectory_test" in sys.argv:
+            mission = "mission_trajectory_test"
+            sys.argv.remove("trajectory_test")
+        elif "takeoff_land_test" in sys.argv:
+            mission = "mission_takeoff_land_test"
+            sys.argv.remove("takeoff_land_test")
+        elif "takeoff_test" in sys.argv:
+            mission = "mission_takeoff_test"
+            sys.argv.remove("takeoff_test")
+        elif "takeoff_all_test" in sys.argv:
+            mission = "mission_takeoff_all_test"
+            sys.argv.remove("takeoff_all_test")
+        elif "land_test" in sys.argv:
+            mission = "mission_land_test"
+            sys.argv.remove("land_test")
+        elif "land_all_test" in sys.argv:
+            mission = "mission_land_all_test"
+            sys.argv.remove("land_all_test")
+        elif "formation_test" in sys.argv:
+            mission = "mission_formation_test"
+            sys.argv.remove("formation_test")
 
         # Launch a mission if a message exists
         if mission == "mission_takeoff_all":
@@ -203,7 +175,9 @@ def main() -> None:
 
         elif mission == "mission_takeoff_all_test":
             # TODO: To it in a loop
-            missionControl.takeOffAll()
+            while i in range(10):
+                missionControl.takeOffAll()
+                missionControl.landAll()
         
         elif mission == "mission_land_test":
             # TODO: To it in a loop
@@ -218,8 +192,10 @@ def main() -> None:
                 
             missionControl.takeOffAll()
             missionControl.testFormation()
-
-        crazySwarm.timeHelper.sleep(1 / 100)
+            #missionControl.landAll()
+            crazySwarm.timeHelper.sleep(1 / 100)
+        
+            
         
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
